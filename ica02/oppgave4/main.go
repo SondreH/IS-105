0b630a689acc65083af50bf1cbb9d45ee2276879
karlosmartines <lund.karlmartin@gmail.com>
2019-02-21 14:31:33 +0200

    oppgave4 2102

---
+++ ica02/oppgave4/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"./unicode"
+)
+
+func main() {
+	fmt.Println(unicode.Translate("“nord og sør”", "is"))
+}	
---
+++ ica02/oppgave4/unicode/unicode.go
@@ -0,0 +1,28 @@
+package unicode
+
+import (
+  "fmt"
+)
+// Kode for Oppgave 4a
+func Translate(expression string, language string) string {
+  if language == "is" {
+    return "\xE2\x80\x9C\x6E\x6F\x72\xC3\xB0\x75\x72\x20\x6F\x67\x20\x73\x75\xC3\xB0\x75\x72\xE2\x80\x9D\n"
+  }
+  if language == "jp" {
+    return "\xE2\x80\x9C\xE5\x8C\x97\xE3\x81\xA8\xE5\x8D\x97\xE2\x80\x9D\n"
+  }
+	return "alien language"
+}
+
+// Kode for Oppgave 4b
+func UnicodeCodePointDemo() {
+	// Hva er dette?
+	// Er det likt på MS Windows og macOS?
+  fmt.Println("\xf0\x9F\x98\x80")
+  fmt.Println("\xf0\x9F\x98\x97")
+  // Demonstrerer at deler av et tegn representert med flere bytes
+  // kan ikke tolkes innenfor koden (unicode)
+  fmt.Println("\xf0\x9F\x98")
+  fmt.Println("\xf0\x9F")
+  fmt.Println("\xf0")
+}
