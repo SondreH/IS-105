a786f8639949b2a36e90c6c13222aa21d963ecdb
Andy@Perez-PC <andrpe18@uia.no>
2019-02-21 14:31:25 +0100

    Oppgave1

---
+++ ica02/02_oppgave01/oppgave1c/ascii/ascii_test.go
@@ -0,0 +1,24 @@
+package ascii
+
+import (
+	"testing"
+	"unicode"
+)
+
+//Oppgave1c
+
+func TestGreetingASCII(t *testing.T) {
+	b := GreetingASCII()
+	if isASCII(b) == false {
+		t.Errorf("FAIL")
+	}
+}
+
+func isASCII(t2 string) bool {
+	for _, i := range t2 {
+		if i > unicode.MaxASCII {
+			return false
+		}
+	}
+	return true
+}
