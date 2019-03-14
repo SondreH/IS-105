3f7bfe66a42528a99e078761cbdc92a9d64e864e
karlosmartines <lund.karlmartin@gmail.com>
2019-03-14 13:02:48 +0100

    riktig path

--- ica03/oppgave5/oppgave5http/httpserver/httpserver.go
+++ ica03/oppgave5/oppgave5http/httpserver/httpserver.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/karlosmartines/karls-windows/Golang/is105-ica03/oppgave5tcp/jsonemail"
+	"github.com/shammers95/IS-105/ica03/oppgave5/oppgave5tcp/jsonemail"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
