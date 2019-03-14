1aa4cdd19849797f1150091b25423c6673820b75
Khai <khaihjn@gmail.com>
2019-03-14 12:57:21 +0100

    Skrt3

--- ica03/Oppgave2/fileinfo/fileinfo.go
+++ ica03/Oppgave2/fileinfo/fileinfo.go
@@ -36,38 +36,38 @@ func main() {
 
 	mode := file.Mode()
 	if mode&os.ModeDir != 0 {
-		fmt.Println("Is a directory file")
+		fmt.Println("Is a directory file.")
 	} else {
-		fmt.Println("Is not a directory file")
+		fmt.Println("Is not a directory file.")
 	}
 	if mode.IsRegular() != false {
-		fmt.Println("Is a regular file")
+		fmt.Println("Is a regular file.")
 	} else {
-		fmt.Println("Is not a regular file")
+		fmt.Println("Is not a regular file.")
 	}
 	fmt.Println("Has Unix permission bits:", mode)
 	if mode&os.ModeAppend != 0 {
-		fmt.Println("Is an append file")
+		fmt.Println("Is an append file.")
 	} else {
-		fmt.Println("Is not an append file")
+		fmt.Println("Is not an append file.")
 	}
 	if mode&os.ModeDevice != 0 {
 		fmt.Println("Is a device file")
 		if mode&os.ModeCharDevice != 0 {
-			fmt.Println("Is a Unix character device file")
-			fmt.Println("Is not a Unix block device file")
+			fmt.Println("Is a Unix character device file.")
+			fmt.Println("Is not a Unix block device file.")
 		} else {
-			fmt.Println("Is not a Unix character device file")
-			fmt.Println("Is a Unix block device file")
+			fmt.Println("Is not a Unix character device file.")
+			fmt.Println("Is a Unix block device file.")
 		}
 	} else {
-		fmt.Println("Is not a device file")
-		fmt.Println("Is not a Unix character device file")
-		fmt.Println("Is not a Unix block device file")
+		fmt.Println("Is not a device file.")
+		fmt.Println("Is not a Unix character device file.")
+		fmt.Println("Is not a Unix block device file.")
 	}
 	if mode&os.ModeSymlink != 0 {
-		fmt.Println("Is a symbolic link")
+		fmt.Println("Is a symbolic link.")
 	} else {
-		fmt.Println("Is not a symbolic link")
+		fmt.Println("Is not a symbolic link.")
 	}
 }
