37e2ad9862df5e8e1d436ead9ac55d87112e2702
Shammers95 <sondreh@live.no>
2019-03-14 12:46:22 +0100

    Finally. ♪♪ Hooked on a feelin'♪♪

--- ica03/Oppgave1/fileutils_main.go
+++ ica03/Oppgave1/fileutils_main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"IS-105/ica03/Oppgave1/fileutils"
+	"github.com/shammers95/IS-105/ica03/Oppgave1/fileutils"
 )
 
 func main() {
 	fileutils.FileToByteslice("./fileutils/text1.txt")
+	fileutils.FileToByteslice("./fileutils/text2.txt")
 	//Kaller metoden FileToByteslice med
-	//parameter text1.txt fra mappen fileutils.
+	//parameter text1.txt og text2.txt fra mappen fileutils.
 }
--- ica03/Oppgave1/lineshift/linjeskift.go
+++ ica03/Oppgave1/lineshift/linjeskift.go
@@ -9,7 +9,7 @@ func Lineshift(etArray []byte) {
 	i := len(etArray) - 2
 	//Sjekke lengden på arrayet og flytte indeksen 2 plasser tilbake
 	arrLinjeNestSist := etArray[i]
-	siste := fmt.Sprintf("% X\n", arrLinjeNestSist) //For å sjekke om
+	siste := fmt.Sprintf("%X\n", arrLinjeNestSist) //For å sjekke om
 	/*det nestsiste ASCII-symbolet har 0D eller 0A/annet.
 	(https://en.wikipedia.org/wiki/Newline
 	TrimSpace fjerner alt av whitespacing for så å sjekke om
@@ -17,7 +17,7 @@ func Lineshift(etArray []byte) {
 	hvorav D tilsvarer linjeskift (CR, Carriage Return).
 	Hvis siste linje har linjeskift printes beskjeden)*/
 
-	fmt.Printf("% X\n", etArray) //Enklere å dobbelsjekke & teste,
+	fmt.Printf("%X\n", etArray) //Enklere å dobbelsjekke & teste,
 	//printer UTF-8 symbolene for hver av tegnene.
 
 	if strings.TrimSpace(siste) == "D" {
