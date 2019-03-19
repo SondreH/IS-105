c9b26cc2b2e8382d4c9e8e64d6f64a0916fa5354
Shammers95 <sondreh@live.no>
2019-03-19 13:22:42 +0100

    Oppdatert til å vise type CR og LF ♪♪ Carry on, carry on, the champion has begun ♪♪

--- ica03/Oppgave1/lineshift/linjeskift.go
+++ ica03/Oppgave1/lineshift/linjeskift.go
@@ -7,30 +7,31 @@ import (
 
 func Lineshift(etArray []byte) {
 	i := len(etArray) - 2
-	//Sjekke lengden på arrayet og flytte indeksen 2 plasser tilbake
+	i2 := len(etArray) - 1
+	//Sjekke i sjekker lengden på arrayet og flytte indeksen 2 plasser tilbake.
 	arrLinjeNestSist := etArray[i]
-	siste := fmt.Sprintf("%X\n", arrLinjeNestSist) //For å sjekke om
-	/*det nestsiste ASCII-symbolet har 0D eller 0A/annet.
-	(https://en.wikipedia.org/wiki/Newline
-	TrimSpace fjerner alt av whitespacing for så å sjekke om
+	arrLinjeSist := etArray[i2]
+	nestSiste := fmt.Sprintf("%X\n", arrLinjeNestSist)
+	siste := fmt.Sprintf("%X\n", arrLinjeSist) //For å sjekke om
+	/*det nestsiste og siste ASCII-symbolet har 0D eller 0A, eller annet.
+	(https://en.wikipedia.org/wiki/Newline */
+	fmt.Print("ASCII-representasjon: ")
+	fmt.Printf("%X\n", etArray) //Enklere å dobbelsjekke & teste,
+	//printer ASCII symbolene for hver av tegnene.
+	fmt.Println("Trimspace returnerer for nestsiste linje:", strings.TrimSpace(nestSiste))
+	fmt.Println("Trimspace returnerer for siste linje:", strings.TrimSpace(siste))
+
+	/*TrimSpace fjerner alt av whitespacing for så å sjekke om
 	A tilsvarer å bevege seg en linje forover (LF, Line feed),
 	hvorav D tilsvarer linjeskift (CR, Carriage Return).
 	Hvis siste linje har linjeskift printes beskjeden)*/
-
-	fmt.Printf("%X\n", etArray) //Enklere å dobbelsjekke & teste,
-	//printer UTF-8 symbolene for hver av tegnene.
-
-	if strings.TrimSpace(siste) == "D" {
+	if strings.TrimSpace(nestSiste) == "D" || strings.TrimSpace(siste) == "D" {
 		/*TrimSpace fjerner alt av whitespacing for så å sjekke om
-		D tilsvarer linjeskift (CR, Carriage Return).
-		Hvis siste linje har linjeskift printes beskjeden*/
-		fmt.Println("Det er med linjeskift.")
+		D tilsvarer linjeskift (CR, Carriage Return). Dersom den tilsvarer A inneholder den en ny linje (LF, Linefeed)*/
+		fmt.Println("Det er med linjeskift (CR, Carriage Return).")
+	} else if (strings.TrimSpace(siste) == "A" || strings.TrimSpace(nestSiste) == "A") && (strings.TrimSpace(siste) != "D" || strings.TrimSpace(nestSiste) != "D") {
+		fmt.Println("Dette er med Linefeed (LF) uten CR")
 	} else {
-		fmt.Println("Det er ikke med linjeskift.")
+		fmt.Println("Dette er i et annet format enn CR / LF.")
 	}
-
-	/*etArrayString := string(etArray)
-	var a = "%\n"
-	containLineshift := fmt.Sprintf(a, etArray)
-	fmt.Println("Inneholder denne filen linjeskift?", containLineshift)*/
 }
--- ica03/oppgave5/oppgave5tcp/tcp_main.go
+++ ica03/oppgave5/oppgave5tcp/tcp_main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/shammers95/IS-105/ica03/oppgave5/oppgave5tcp/tcpclient"
 	"github.com/shammers95/IS-105/ica03/oppgave5/oppgave5tcp/tcpserver"
 )
 
 func main() {
-	go tcpclient.Dial()
+	//go tcpclient.Dial()
 	tcpserver.ServeTCP()
 
 }
