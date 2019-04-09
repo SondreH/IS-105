872b8a7759a12e76615e6ae80ba2632d0ab6d5ed
Shammers95 <sondreh@live.no>
2019-04-09 12:20:46 +0200

    ica04-03-del3-ferdig

--- ica04/Oppgave3/speech_main.go
+++ ica04/Oppgave3/speech_main.go
@@ -1,9 +1,13 @@
 package main
 
 //import (
-
+//	"github.com/SondreH/IS-105/ica04/Oppgave3/gspeech-rec-master/"
+//	"github.com/SondreH/IS-105/ica04/Oppgave3/go-speak-master"
 //)
 
 func main() {
+	continuous.start()
 
+	wit.SetWitKey("qP7byWDB8PjOvVPeGPdLkS-6pvOj_DBP_F4WkQo5dkhm")
+	wit.SendWitVoice("github.com/SondreH/IS-105/ica04/Oppgave3/gspeech-rec-master/hellothisiskarlmartin.m4a")
 }
